ca3010d524edba98a7e0f63652c5ee075fde8d22
Liony427 <jhlim0427@naver.com>
2023-07-07 19:27:49 +0900

    회원가입 API 작성 완료
    - select
    - update
    - delete
    - import

---
+++ src/main/java/com/ssamz/jblog/entity/RoleType.java
@@ -0,0 +1,7 @@
+package com.ssamz.jblog.entity;
+
+ 
+
+public enum RoleType {
+  USER, ADMIN;
+}
---
+++ src/main/java/com/ssamz/jblog/service/UserService.java
@@ -0,0 +1,65 @@
+package com.ssamz.jblog.service;
+
+import org.springframework.stereotype.Service;
+
+import com.ssamz.jblog.entity.UserEntity;
+import com.ssamz.jblog.exception.JBlogException;
+import com.ssamz.jblog.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+
+@RequiredArgsConstructor
+
+public class UserService {
+
+	private final UserRepository repository;
+
+	public UserEntity findById(String id) {
+
+		return repository.findById(Long.parseLong(id)).orElseThrow(() -> {
+
+			return new JBlogException(id + "를 가진 회원이 존재하지 않습니다");
+
+		});
+
+	}
+
+	public UserEntity update(UserEntity oldUser, UserEntity newUser) {
+
+		UserEntity user = repository.findById(oldUser.getId()).orElseThrow(() -> {
+
+			return new JBlogException(oldUser.getId() + "를 가진 회원이 존재하지 않습니다");
+
+		});
+		
+		
+		user.setEmail(newUser.getEmail());
+		user.setUsername(newUser.getUsername());
+		user.setPassword(newUser.getPassword());
+		repository.save(user);
+		return user;
+
+	}
+	
+public UserEntity insertUser(UserEntity user) {
+
+    return repository.save(user);
+
+}
+
+
+public void deleteById(String id) {
+	UserEntity user = repository.findById(Long.parseLong(id)).orElseThrow(() -> {
+
+        return new JBlogException(id + "를 가진 회원이 존재하지 않습니다");    
+
+    });
+
+    repository.delete(user);
+
+}
+
+}
+
